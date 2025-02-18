4ae46f091988dcec7c7a5f4d12121ebae1cac6a2
ricardo <1221720@isep.ipp.pt>
2025-02-18 19:11:19 +0000

    Changed Task attribute "completed" to not be required

--- models/Task.js
+++ models/Task.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const TaskSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    completed: { type: Boolean, default: false, required: true }
+    completed: { type: Boolean, default: false }
 });
 
 const Task = mongoose.model('Task', TaskSchema);
