c4ab452e2d9d7834b45fcdfbea4dad2eb9c9e37b
ricardo <1221720@isep.ipp.pt>
2025-02-19 10:12:38 +0000

    Created Tasks Page

--- Frontend/src/app/tasks/TaskService.js
+++ Frontend/src/app/tasks/TaskService.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api/tasks';
+const API_URL = 'http://192.168.1.198:5001/api/tasks';
 
-export const getAllTasks = () => axios.get(API_URL);
-export const createTask = (title) => axios.post(API_URL, { title });
-export const toggleTaskCompletion = (id) => axios.patch(`${API_URL}/${id}`);
+export const fetchTasks = () => axios.get(API_URL);
+export const addTask = (title) => axios.post(API_URL, { title });
+export const toggleTask = (id) => axios.patch(`${API_URL}/${id}`);
 export const deleteTask = (id) => axios.delete(`${API_URL}/${id}`);
