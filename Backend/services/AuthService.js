ef0563ab52c46fdfcd0bcbca69569d668177d07c
ricardo <1221720@isep.ipp.pt>
2025-02-20 12:46:36 +0000

    Added JWT_KEY to env

--- Backend/services/AuthService.js
+++ Backend/services/AuthService.js
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import * as AuthRepository from '../repositories/AuthRepository.js';
 
-const SECRET_KEY = 'secret_key';
+const JWT_KEY = process.env.JWT_KEY;
 
 export const register = async (username, password) => {
     const existingUser = await AuthRepository.findUserByUsername(username);
@@ -19,6 +19,6 @@ export const login = async (username, password) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) throw new Error('Invalid credentials');
 
-    const token = jwt.sign({ userId: user._id, username: user.username }, SECRET_KEY, { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user._id, username: user.username }, JWT_KEY, { expiresIn: '1h' });
     return token;
 };
