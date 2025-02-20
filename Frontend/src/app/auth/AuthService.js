00c44650281c0cd717ba1e9f9052f96d44e5ae94
ricardo <1221720@isep.ipp.pt>
2025-02-20 13:40:55 +0000

    Added VITE_API_URL to env

--- Frontend/src/app/auth/AuthService.js
+++ Frontend/src/app/auth/AuthService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://backend-desafiotasks.onrender.com/api/auth';
+const API_URL = '${process.env.VITE_API_URL}/auth';
 
 export const registerUser = async ({ username, password }) => {
     return await axios.post(`${API_URL}/register`, { username, password });
--- Frontend/src/app/tasks/TaskService.js
+++ Frontend/src/app/tasks/TaskService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://backend-desafiotasks.onrender.com/api/tasks';
+const API_URL = '${process.env.VITE_API_URL}/tasks';
 
 export const fetchTasks = () => axios.get(API_URL);
 export const addTask = (title) => axios.post(API_URL, { title });
